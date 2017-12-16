bf45c1165ead1b3f5211b3fedad0db4cb171cb4c
Jérémie Astori <jeremie@astori.fr>
2017-12-16 16:19:51 -0500

    Improve error page at loading
    - Display the "Reload page" instantly and not after 5 seconds
    - Fix missing stack trace
    - Wrap the stack trace so it does not expand beyond boundaries (scrollbar would not show up either)
    - Do not show the slow-loading warning on error
    - Make stack trace selectable instead of editable
    - Label improvements

--- client/js/loading-slow-alert.js
+++ client/js/loading-slow-alert.js
@@ -8,40 +8,53 @@
  *    so that the timeout can be triggered while slow JS is loading
  */
 
-setTimeout(function() {
-	var element = document.getElementById("loading-slow");
-
-	if (element) {
-		element.style.display = "block";
+function displayReload() {
+	var loadingReload = document.getElementById("loading-reload");
+	if (loadingReload) {
+		loadingReload.style.display = "block";
 	}
+}
+
+var loadingSlowTimeout = setTimeout(function() {
+	var loadingSlow = document.getElementById("loading-slow");
+
+	if (loadingSlow) {
+		loadingSlow.style.display = "block";
+	}
+
+	displayReload();
 }, 5000);
 
-document.getElementById("loading-slow-reload").addEventListener("click", function() {
+document.getElementById("loading-reload").addEventListener("click", function() {
 	location.reload();
 });
 
-window.g_LoungeErrorHandler = function LoungeErrorHandler(error) {
+window.g_LoungeErrorHandler = function LoungeErrorHandler(e) {
 	var title = document.getElementById("loading-title");
 	title.textContent = "An error has occured";
 
-	title = document.getElementById("loading-page-message");
-	title.textContent = "An error has occured that prevented the client from loading correctly.";
+	var message = document.getElementById("loading-page-message");
+	message.textContent = "An error has occured that prevented the client from loading correctly.";
 
 	var summary = document.createElement("summary");
 	summary.textContent = "More details";
 
-	if (error instanceof ErrorEvent) {
-		error = error.message + "\n\n" + error.stack + "\n\nView developer tools console for more information and a better stacktrace.";
+	var data = document.createElement("pre");
+	if (e instanceof ErrorEvent) {
+		data.textContent = e.error && e.error.stack ? e.error.stack : e.message;
 	}
 
-	var data = document.createElement("pre");
-	data.contentEditable = true;
-	data.textContent = error;
+	var info = document.createElement("p");
+	info.textContent = "Open your browser developer tools for more information.";
 
 	var details = document.createElement("details");
 	details.appendChild(summary);
 	details.appendChild(data);
-	title.parentNode.insertBefore(details, title.nextSibling);
+	details.appendChild(info);
+	message.parentNode.insertBefore(details, message.nextSibling);
+
+	window.clearTimeout(loadingSlowTimeout);
+	displayReload();
 };
 
 window.addEventListener("error", window.g_LoungeErrorHandler);
