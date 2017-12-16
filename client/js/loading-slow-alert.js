016ec0751f74fb9507f21de7c77ce995a0703922
Jérémie Astori <jeremie@astori.fr>
2017-12-16 18:14:58 -0500

    Wrap entire error loading script in anonymous function to avoid leaks

--- client/js/loading-slow-alert.js
+++ client/js/loading-slow-alert.js
@@ -8,53 +8,55 @@
  *    so that the timeout can be triggered while slow JS is loading
  */
 
-function displayReload() {
-	var loadingReload = document.getElementById("loading-reload");
-	if (loadingReload) {
-		loadingReload.style.display = "block";
-	}
-}
+(function() {
+	var displayReload = function displayReload() {
+		var loadingReload = document.getElementById("loading-reload");
+		if (loadingReload) {
+			loadingReload.style.display = "block";
+		}
+	};
 
-var loadingSlowTimeout = setTimeout(function() {
-	var loadingSlow = document.getElementById("loading-slow");
+	var loadingSlowTimeout = setTimeout(function() {
+		var loadingSlow = document.getElementById("loading-slow");
 
-	if (loadingSlow) {
-		loadingSlow.style.display = "block";
-	}
+		if (loadingSlow) {
+			loadingSlow.style.display = "block";
+		}
 
-	displayReload();
-}, 5000);
+		displayReload();
+	}, 5000);
 
-document.getElementById("loading-reload").addEventListener("click", function() {
-	location.reload();
-});
+	document.getElementById("loading-reload").addEventListener("click", function() {
+		location.reload();
+	});
 
-window.g_LoungeErrorHandler = function LoungeErrorHandler(e) {
-	var title = document.getElementById("loading-title");
-	title.textContent = "An error has occured";
+	window.g_LoungeErrorHandler = function LoungeErrorHandler(e) {
+		var title = document.getElementById("loading-title");
+		title.textContent = "An error has occured";
 
-	var message = document.getElementById("loading-page-message");
-	message.textContent = "An error has occured that prevented the client from loading correctly.";
+		var message = document.getElementById("loading-page-message");
+		message.textContent = "An error has occured that prevented the client from loading correctly.";
 
-	var summary = document.createElement("summary");
-	summary.textContent = "More details";
+		var summary = document.createElement("summary");
+		summary.textContent = "More details";
 
-	var data = document.createElement("pre");
-	if (e instanceof ErrorEvent) {
-		data.textContent = e.error && e.error.stack ? e.error.stack : e.message;
-	}
+		var data = document.createElement("pre");
+		if (e instanceof ErrorEvent) {
+			data.textContent = e.error && e.error.stack ? e.error.stack : e.message;
+		}
 
-	var info = document.createElement("p");
-	info.textContent = "Open your browser developer tools for more information.";
+		var info = document.createElement("p");
+		info.textContent = "Open your browser developer tools for more information.";
 
-	var details = document.createElement("details");
-	details.appendChild(summary);
-	details.appendChild(data);
-	details.appendChild(info);
-	message.parentNode.insertBefore(details, message.nextSibling);
+		var details = document.createElement("details");
+		details.appendChild(summary);
+		details.appendChild(data);
+		details.appendChild(info);
+		message.parentNode.insertBefore(details, message.nextSibling);
 
-	window.clearTimeout(loadingSlowTimeout);
-	displayReload();
-};
+		window.clearTimeout(loadingSlowTimeout);
+		displayReload();
+	};
 
-window.addEventListener("error", window.g_LoungeErrorHandler);
+	window.addEventListener("error", window.g_LoungeErrorHandler);
+})();
